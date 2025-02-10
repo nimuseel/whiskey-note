0a50a5802abd5d16aacd790fd11b7f96ab91efcf
nimuseel <l08c0739@gmail.com>
2025-02-10 15:31:15 +0900

    Initial Commit

---
+++ whiskey noteTests/whiskey_noteTests.swift
@@ -0,0 +1,17 @@
+//
+//  whiskey_noteTests.swift
+//  whiskey noteTests
+//
+//  Created by 이수민 on 2/10/25.
+//
+
+import Testing
+@testable import whiskey_note
+
+struct whiskey_noteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ whiskey noteUITests/whiskey_noteUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  whiskey_noteUITestsLaunchTests.swift
+//  whiskey noteUITests
+//
+//  Created by 이수민 on 2/10/25.
+//
+
+import XCTest
+
+final class whiskey_noteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
