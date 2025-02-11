14d5ff296ab0a06c46913e9fe47f0c42ec86e5b5
nimuseel <l08c0739@gmail.com>
2025-02-11 11:24:05 +0900

    chore: 프로젝트 이름 변경

---
+++ whiskey note/Aroma.swift
@@ -0,0 +1,8 @@
+//
+//  Aroma.swift
+//  whiskey note
+//
+//  Created by 이수민 on 2/11/25.
+//
+
+import Foundation
---
+++ whiskey note/Mouthfeel.swift
@@ -0,0 +1,8 @@
+//
+//  Mouthfeel.swift
+//  whiskey note
+//
+//  Created by 이수민 on 2/11/25.
+//
+
+import Foundation
