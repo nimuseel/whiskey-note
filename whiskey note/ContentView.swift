65d73cf9a508e4f93ce51a4fa63dff3f3e7817ad
nimuseel <l08c0739@gmail.com>
2025-02-18 12:00:12 +0900

    feat: LaunchView 연결

--- whiskey note/ContentView.swift
+++ whiskey note/ContentView.swift
@@ -5,35 +5,50 @@ struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var tasteNotes: [TasteNote]
     @State private var isCreateViewPresent = false
+    
+    @State private var showMainView: Bool = false
 
     var body: some View {
-        NavigationStack {
-            List {
-                ForEach(tasteNotes.sorted(by: { $0.createdAt < $1.createdAt })) { tasteNote in
-                    NavigationLink {
-                        UpdateView(tasteNote: .constant(tasteNote))
-                    } label: {
-                        VStack(alignment: .leading) {
-                            HStack(spacing: 4) {
-                                Text(tasteNote.whiskey.name).font(.subheadline)
-                                Text("/ \(tasteNote.whiskey.category)").font(.subheadline)
+        ZStack {
+            if showMainView {
+                NavigationStack {
+                    List {
+                        ForEach(tasteNotes.sorted(by: { $0.createdAt < $1.createdAt })) { tasteNote in
+                            NavigationLink {
+                                UpdateView(tasteNote: .constant(tasteNote))
+                            } label: {
+                                VStack(alignment: .leading) {
+                                    HStack(spacing: 4) {
+                                        Text(tasteNote.whiskey.name).font(.subheadline)
+                                        Text("/ \(tasteNote.whiskey.category)").font(.subheadline)
+                                    }
+                                    Text(tasteNote.dish).font(.footnote).foregroundColor(.secondary)
+                                }
                             }
-                            Text(tasteNote.dish).font(.footnote).foregroundColor(.secondary)
+                        }
+                        .onDelete(perform: deleteItems)
+                    }
+                    .toolbar {
+                        ToolbarItem {
+                            Button("추가하기") {
+                                isCreateViewPresent = true
+                            }
                         }
                     }
+                    .navigationDestination(isPresented: $isCreateViewPresent, destination: {
+                        CreateView()
+                    })
                 }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem {
-                    Button("추가하기") {
-                        isCreateViewPresent = true
+            } else {
+                LaunchView()
+                    .onAppear {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                            withAnimation {
+                                showMainView = true
+                            }
+                        }
                     }
-                }
             }
-            .navigationDestination(isPresented: $isCreateViewPresent, destination: {
-                CreateView()
-            })
         }
     }
     
