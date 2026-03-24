c1c5da6ed3071b4b2b75d485b1489dc517638632
nimusee1 <l08c0739@gmail.com>
2026-03-24 09:51:49 +0900

    refactor: use manual ModelContainer to support screenshot data injection

--- whiskey note/whiskey_noteApp.swift
+++ whiskey note/whiskey_noteApp.swift
@@ -3,10 +3,20 @@ import SwiftData
 
 @main
 struct whiskey_noteApp: App {
+    let container: ModelContainer = {
+        let schema = Schema([WhiskeyNote.self, FlavorIntensity.self])
+        return try! ModelContainer(for: schema)
+    }()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onAppear {
+                    if ProcessInfo.processInfo.arguments.contains("-SCREENSHOT_MODE") {
+                        ScreenshotSeedData.seed(into: container.mainContext)
+                    }
+                }
         }
-        .modelContainer(for: [WhiskeyNote.self, FlavorIntensity.self])
+        .modelContainer(container)
     }
 }
